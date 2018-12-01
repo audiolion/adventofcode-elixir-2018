467242e581f3b6d375ea46cb0a5a667f4dee685b
Ryan Castner <castner.rr@gmail.com>
2018-12-01 16:28:57 -0500

    Input 2, solution 2

---
+++ 01/solution-2.exs
@@ -0,0 +1,21 @@
+{:ok, input } = File.read Path.expand("./input-2.txt")
+data = String.split(input, "\n", [])
+
+defmodule AOC.Day1 do
+  def find_frequency(data) do
+    stream = Stream.cycle(data)
+    Enum.reduce_while(stream, %{frequency: 0, frequencies: MapSet.new},
+      fn(x, %{frequency: frequency, frequencies: frequencies}) ->
+        {num, _ } = Integer.parse(x)
+        next_frequency = num + frequency
+        case MapSet.member?(frequencies, next_frequency) do
+          true -> {:halt, next_frequency}
+          false -> {:cont, %{frequency: next_frequency, frequencies: MapSet.put(frequencies, next_frequency)}}
+        end
+      end
+    )
+  end
+end
+
+result = AOC.Day1.find_frequency(data)
+IO.inspect result
