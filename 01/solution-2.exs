919a8a0457ce3cd0547690fa4e04d4246a815f4a
Ryan Castner <castner.rr@gmail.com>
2018-12-02 18:00:10 -0500

    Formatting

--- 01/solution-2.exs
+++ 01/solution-2.exs
@@ -1,16 +1,28 @@
-{:ok, input } = File.read Path.expand("./input-2.txt")
+{:ok, input} = File.read(Path.expand("./input-2.txt"))
 data = String.split(input, "\n", [])
 
 defmodule AOC.Day1 do
   def find_frequency(data) do
     stream = Stream.cycle(data)
-    Enum.reduce_while(stream, %{frequency: 0, frequencies: MapSet.new},
-      fn(x, %{frequency: frequency, frequencies: frequencies}) ->
-        {num, _ } = Integer.parse(x)
+
+    Enum.reduce_while(
+      stream,
+      %{frequency: 0, frequencies: MapSet.new()},
+      fn x,
+         %{
+           frequency: frequency,
+           frequencies: frequencies
+         } ->
+        {num, _} = Integer.parse(x)
         next_frequency = num + frequency
+
         case MapSet.member?(frequencies, next_frequency) do
-          true -> {:halt, next_frequency}
-          false -> {:cont, %{frequency: next_frequency, frequencies: MapSet.put(frequencies, next_frequency)}}
+          true ->
+            {:halt, next_frequency}
+
+          false ->
+            {:cont,
+             %{frequency: next_frequency, frequencies: MapSet.put(frequencies, next_frequency)}}
         end
       end
     )
@@ -18,4 +30,4 @@ defmodule AOC.Day1 do
 end
 
 result = AOC.Day1.find_frequency(data)
-IO.inspect result
+IO.puts(result)
--- 01/solution.exs
+++ 01/solution.exs
@@ -1,9 +1,10 @@
-{:ok, input } = File.read Path.expand("./input.txt")
+{:ok, input} = File.read(Path.expand("./input.txt"))
 data = String.split(input, "\n")
-sum = Enum.reduce(data, 0,
-  fn(x, acc) ->
-    {num, _ } = Integer.parse(x)
+
+sum =
+  Enum.reduce(data, 0, fn x, acc ->
+    {num, _} = Integer.parse(x)
     num + acc
-  end
-)
-IO.inspect sum
+  end)
+
+IO.put(sum)
